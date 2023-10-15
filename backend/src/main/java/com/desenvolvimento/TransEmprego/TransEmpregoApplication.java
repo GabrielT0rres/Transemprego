4a941322f3f8daf4cea702d1c7f8454973fcb6b9
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2023-10-15 20:26:40 -0300

    adicionado o backend

---
+++ backend/src/main/java/com/desenvolvimento/TransEmprego/TransEmpregoApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.TransEmprego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransEmpregoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransEmpregoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/desenvolvimento/TransEmprego/TransEmpregoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.TransEmprego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransEmpregoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
