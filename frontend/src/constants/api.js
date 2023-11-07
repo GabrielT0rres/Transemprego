93b4b42908f3e746ccc66e888634d1564f8f812a
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2023-11-06 23:16:30 -0300

    adicionado nova url do Backend

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,5 +1,6 @@
 //const URL_API = 'http://localhost:8080';
-const URL_API = 'https://41jx7vg3-8080.brs.devtunnels.ms';
+// const URL_API = 'https://41jx7vg3-8080.brs.devtunnels.ms';
+const URL_API = 'https://trans-prego.onrender.com'
 const USERNAME_API = 'myclientid'
 const PASSWORD_API = 'myclientsecret';
 
